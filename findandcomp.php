eb63c0674b381a2f1dc96e976cc066c51dd22eb4
brunellamarzolini <brunellamarzolini@ab-acus.eu>
2018-12-27 10:12:27 +0100

    html structure set

--- findandcomp.php
+++ findandcomp.php
@@ -1,3 +1,18 @@
-<?php
-// hello.php
-echo 'Hello, world!';
+<html>
+
+<head>
+   <title>Find & Compare</title>
+</head>
+
+<body>
+    <br>
+        website 1: <input type="url" name="website1"><br>  <!--qui non mi era chiaro se dovessi per forza usare un type="text"-->
+    <br>
+        website 2: <input type="url" name="website2"><br>
+    <br>
+    <br>
+        <button type="submit" name="submit">Find&Compare</button>
+    <br>
+    </form>
+</body>
+</html> 
